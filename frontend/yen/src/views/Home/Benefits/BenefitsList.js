0dd481a642a8bcc8b76ee69c26e96451b79f25b0
liam9408 <57433615+liam9408@users.noreply.github.com>
2020-06-08 16:23:39 +0800

    added membership card and benefits list

--- frontend/yen/src/views/Home/Benefits/BenefitsList.js
+++ frontend/yen/src/views/Home/Benefits/BenefitsList.js
@@ -2,6 +2,104 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import * as benefitsActions from "../../../store/actions/benefits/benefitsActions";
+import { withRouter } from "react-router-dom";
+
+// ---- styling
+const cardBody = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  marginTop: "20px",
+  // width: "50%",
+};
+
+const benefitsCard = {
+  backgroundColor: "white",
+  // border: "1px solid black",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+  borderRadius: "10px",
+  // webkitBoxShadow: "0px 0px 13px 3px rgba(0, 0, 0, 0.08)",
+  // mozBoxShadow: "0px 0px 13px 3px rgba(0, 0, 0, 0.08)",
+  // boxShadow: "0px 0px 13px 3px rgba(0, 0, 0, 0.08)",
+};
+
+const benefitsContent = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "flex-start",
+  alignContent: "space-between",
+  width: "100%",
+};
+
+const benefitsTop = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "flex-end",
+};
+
+const benefitsDetails = {
+  display: "flex",
+  flexDirection: "column",
+};
+
+const benefitsTitle = {
+  marginRight: "20px",
+  marginBottom: "10px",
+  marginTop: "20px",
+  fontSize: "15px",
+  fontWeight: "400",
+};
+
+const benefitsTitleText = {
+  marginBottom: "10px",
+  marginTop: "10px",
+  fontSize: "15px",
+};
+
+const benefitsDescriptionText = {
+  marginTop: "0px",
+  marginBottom: "20px",
+  fontSize: "13px",
+  fontWeight: "300",
+};
+
+const lineStyle = {
+  border: "0.5px solid #DCDCDC",
+  width: "100%",
+};
+
+const image = {
+  backgroundColor: "grey",
+  width: "100px",
+  height: "100px",
+  marginTop: "20px",
+  marginRight: "40px",
+  marginLeft: "20px",
+  marginBottom: "20px",
+};
+
+const benefitsListBody = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "flex-start",
+  width: "100%",
+};
+
+const alignContainer = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  width: "50%",
+};
+
+const leftText = {
+  textAlign: "left",
+};
 
 const BenefitsList = (props) => {
   useEffect(() => {
@@ -10,18 +108,39 @@ const BenefitsList = (props) => {
 
   return (
     <>
-      <div id="benefits-list-card">
-        <h5>This is the benefit card</h5>
-        {props.benefitsMSP.map((item, index) => {
-          console.log(item);
-          return (
-            <>
-              <h5>{item.company}</h5>
-              <h5>{item.benefit_title}</h5>
-              <p>{item.benefits_description}</p>
-            </>
-          );
-        })}
+      <div id="benefitsListCard" style={benefitsListBody}>
+        <div id="alignContainer" style={alignContainer}>
+          <h4 id="leftText" style={leftText}>
+            All membership benefits
+          </h4>
+          {props.benefitsMSP.map((item, index) => {
+            console.log(item);
+            return (
+              <>
+                <div id="cardBody" style={cardBody}>
+                  <div id="benefitsCard" style={benefitsCard}>
+                    {/* <img src={item.logo}></img> */}
+                    <div id="benefitsContent" style={benefitsContent}>
+                      <div style={image}></div>
+                      <div id="benefitDetails" style={benefitsDetails}>
+                        <div style={benefitsTop}>
+                          <h1 style={benefitsTitle}>{item.company}</h1>
+                          <h5 style={benefitsTitleText}>
+                            {item.benefit_title}
+                          </h5>
+                        </div>
+                        <p style={benefitsDescriptionText}>
+                          {item.benefits_description}
+                        </p>
+                      </div>
+                    </div>
+                    <hr style={lineStyle} />
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
       </div>
     </>
   );
--- frontend/yen/src/views/Home/Card/Card.js
+++ frontend/yen/src/views/Home/Card/Card.js
@@ -1,11 +1,45 @@
 import React from "react";
+import YenLogo from "./yenlogo.PNG";
 import { connect } from "react-redux";
+
+const cardBody = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  marginBottom: "50px",
+  marginTop: "50px",
+};
+
+const yenLogo = {
+  width: "80px",
+  height: "80px",
+  marginTop: "20px",
+  marginLeft: "20px",
+};
+
+const memberName = {
+  fontSize: "20px",
+  fontWeight: "600",
+  color: "white",
+  marginLeft: "20px",
+  marginBottom: "80px",
+};
+
+const membershipCardContainer = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  // alignContent: "space-between",
+};
 
 const Card = (props) => {
   return (
     <>
-      <div className="card-body">
-        <h1>This is the card</h1>
+      <div style={cardBody} className="card-body">
+        <div id="membershipCard" style={membershipCardContainer}>
+          <img id="yenLogo" style={yenLogo} src={YenLogo} alt="yenLogo"></img>
+          <span style={memberName}>Chan Tai Ming</span>
+        </div>
       </div>
     </>
   );
