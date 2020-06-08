09cec998ef14a515222855a52e013aa3ac4d61ac
FoongL <leung.foong@gmail.com>
2020-06-08 15:50:47 +0800

    authentication and token verification added in

--- backend/router/benefitsRouter.js
+++ backend/router/benefitsRouter.js
@@ -1,5 +1,19 @@
 // Require the necessary modules for this file.
-const express = require("express");
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+// Verify Token
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers.authorization;
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 
 // Setup a JobsServices so we can use it later on
 class BenefitsRouter {
@@ -7,18 +21,23 @@ class BenefitsRouter {
     this.benefitsService = benefitsService;
   }
 
-  // Binding the stuff to the stuff
   router() {
     let router = express.Router();
-    //  router.get('/is-logged-in',authClass.authenticate() ,this.isLoggedIn.bind(this));
-    router.get("/list/all", this.all.bind(this));
+    router.get('/list/all', verifyToken, this.all.bind(this));
     return router;
   }
+
   all(req, res) {
-    return this.benefitsService
-      .listAll(req.params.userid)
-      .then((data) => res.json(data))
-      .catch((err) => res.status(500).json(err));
+    jwt.verify(req.token, process.env.JWT_SECRET, (err) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        return this.benefitsService
+          .listAll(req.params.userid)
+          .then((data) => res.json(data))
+          .catch((err) => res.status(500).json(err));
+      }
+    });
   }
 }
 
--- backend/service/loginService.js
+++ backend/service/loginService.js
@@ -1,67 +1,67 @@
-// const bcrypt = require("../authentication/bcrypt");
-// const jwt = require("jsonWebToken");
+const bcrypt = require('../authentication/bcrypt');
+const jwt = require('jsonWebToken');
 
-// const config = process.env;
+const config = process.env;
 
-// class LoginService {
-//   constructor(knex) {
-//     this.knex = knex;
-//   }
+class LoginService {
+  constructor(knex) {
+    this.knex = knex;
+  }
 
-//   async login(email, password) {
-//     return new Promise(async (resolve, reject) => {
-//       let findUser = this.knex("members")
-//         .select("id", "password")
-//         .where("email", email);
-//       findUser.then(async (data) => {
-//         if (data.length === 0) {
-//           resolve({ success: 0, error: "email not found" });
-//         } else {
-//           let result = await bcrypt.checkPassword(password, data[0].password);
-//           if (result) {
-//             let payload = {
-//               id: data[0].id,
-//               tokenCreatedDate: new Date().getTime(),
-//             };
-//             const token = jwt.encode(payload, config.jwtSecretUsedForSigning);
-//             resolve({ success: 1, token: token, id: data[0].id });
-//           } else {
-//             resolve({ success: 0, error: "password not found" });
-//           }
-//         }
-//       });
-//     });
-//   }
-//   async signUp(fName, lName, email, password) {
-//     return new Promise((resolve, reject) => {
-//       let userCheck = this.knex("members").select("*").where("email", email);
-//       userCheck.then(async (data) => {
-//         if (data.length > 0) {
-//           resolve({ success: 0, error: "email is already in use" });
-//         } else {
-//           let hash = await bcrypt.hashPassword(password);
-//           let insertUser = this.knex
-//             .insert({
-//               f_name: fName,
-//               l_name: lName,
-//               email: email,
-//               password: hash,
-//             })
-//             .into("members");
-//           insertUser.then(() => {
-//             userCheck.then((data) => {
-//               let payload = {
-//                 id: data[0].id,
-//                 tokenCreatedDate: new Date().getTime(),
-//               };
-//               const token = jwt.encode(payload, config.jwtSecretUsedForSigning);
-//               resolve({ success: 1, token: token, id: data[0].id });
-//             });
-//           });
-//         }
-//       });
-//     });
-//   }
-// }
+  async login(email, password) {
+    return new Promise(async (resolve, reject) => {
+      let findUser = this.knex('members')
+        .select('id', 'password')
+        .where('email', email);
+      findUser.then(async (data) => {
+        if (data.length === 0) {
+          resolve({ success: 0, error: 'email not found' });
+        } else {
+          let result = await bcrypt.checkPassword(password, data[0].password);
+          if (result) {
+            let payload = {
+              id: data[0].id,
+            };
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+              expiresIn: '30 days',
+            });
+            resolve({ success: 1, token: token });
+          } else {
+            resolve({ success: 0, error: 'password not found' });
+          }
+        }
+      });
+    });
+  }
+  async signUp(fName, lName, email, password) {
+    return new Promise((resolve, reject) => {
+      let userCheck = this.knex('members').select('*').where('email', email);
+      userCheck.then(async (data) => {
+        if (data.length > 0) {
+          resolve({ success: 0, error: 'email is already in use' });
+        } else {
+          let hash = await bcrypt.hashPassword(password);
+          let insertUser = this.knex
+            .returning(['id'])
+            .insert({
+              f_name: fName,
+              l_name: lName,
+              email: email,
+              password: hash,
+            })
+            .into('members');
+          insertUser.then((data) => {
+            const token = jwt.sign(
+              { userId: data[0].id },
+              process.env.JWT_SECRET,
+              { expiresIn: '30 days' }
+            );
+            resolve({ success: 1, token: token });
+          });
+        }
+      });
+    });
+  }
+}
 
-// module.exports = LoginService;
+module.exports = LoginService;
