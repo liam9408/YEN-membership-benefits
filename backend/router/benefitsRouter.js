2d971760b43340458a142a3127e64ee711c86ec0
Foong <foong@appit.com.hk>
2020-08-05 10:32:42 +0800

    fixed shitty authentication method

--- backend/router/benefitsRouter.js
+++ backend/router/benefitsRouter.js
@@ -6,9 +6,9 @@ const jwt = require('jsonwebtoken');
 function verifyToken(req, res, next) {
   const bearerHeader = req.headers.authorization;
   if (typeof bearerHeader !== 'undefined') {
-    const bearer = bearerHeader.split(' ');
-    const bearerToken = bearer[1];
-    req.token = bearerToken;
+    // const bearer = bearerHeader.split(' ');
+    // const bearerToken = bearer[1];
+    req.token = bearerHeader;
     next();
   } else {
     res.sendStatus(403);
